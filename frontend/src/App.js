5b3d99c9147708bcc67451f96dc967268820c1bb
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-05-04 11:38:47 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TicketForm from "./components/TicketForm";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>Support Triage System</h1>
+      <TicketForm />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
